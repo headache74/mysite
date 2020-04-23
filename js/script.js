98107f09dffa20742c7bb40c7f4d62f327b6aeac
Sergey Noskov <sergeforce@mail.ru>
2020-04-24 00:36:01 +0300

    js for close popup

--- js/script.js
+++ js/script.js
@@ -1,23 +1,17 @@
 $(function(){
+	$(document).ready(function(){
+		$("#callback__phone").mask("+7 (999) 99-99-999");
+	});	
 	//Форма заказа звонка
 	$('.callback-button').click(function(){
 		$('.callback__popup').fadeIn(400);
 	});
-	$('.callback__popup').click(function(event){
+	$('.callback__popup, .callback__form_close-btn').click(function(event){
 		if(event.target == this) {
 			//$(this).hide();
-			$(this).fadeOut(400);
+			$('.callback__popup').fadeOut(400);
 		}
 	});
-	
-	//Удалить
-	//Карусель отзывов
-	function changeReport (repImg, repName, repOrg, repTxt){
-		$('.report__image').attr("src", repImg);
-		$('.report__caption_name').text(repName);
-		$('.report__caption_org').text(repOrg);
-		$('.report__text').text(repTxt);
-	}
 
 	//Мобильное меню
 	$('.header__menu_mob').click(function(){
@@ -25,7 +19,7 @@ $(function(){
 		$('.popup__menu').fadeIn(400);
 	});
 
-	$('.popup__menu, .popup').click(function(){
+	$('.popup__menu, .popup, .popup__menu_close-btn').click(function(){
 
 			$('.popup').fadeOut(400);
 	});
